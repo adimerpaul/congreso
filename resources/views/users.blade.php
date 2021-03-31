3a5215fc8399c6891b90ce4f4b3f01db8fde9b25
adimerpaul <adimer101@email.com>
2021-03-30 20:57:55 -0400

    primera version

---
+++ resources/views/users.blade.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+
+
+<div class="container">
+    <?php foreach ($users as $user): ?>
+        <?php echo $user->name; ?>
+    <?php endforeach; ?>
+</div>
+
+{{ $users->links('pagination') }}
+
+
+
+
+<body>
+
+<!--JavaScript at end of body for optimized loading-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
+
+
+
+
